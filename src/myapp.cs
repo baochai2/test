47566887045fa857337b83cdb03f0e11ef16af91
baochai2 <166940968+baochai2@users.noreply.github.com>
2026-01-09 23:47:12 +0800

    add new func to myapp.cs

--- src/myapp.cs
+++ src/myapp.cs
@@ -3,5 +3,12 @@ partial class Program
     static void Main()
     {
         Console.WriteLine("hello world");
+
+        Greet("Tom");
+    }
+
+    static void Greet(string name)
+    {
+        Console.WriteLine($"Hello, {name}!");
     }
 }
