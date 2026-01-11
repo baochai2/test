ee3db3fcb85cab4653ee192c22fe146eac340601
baochai2 <166940968+baochai2@users.noreply.github.com>
2026-01-11 15:32:02 +0800

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Welcome to my awesome app!');
-});
+  res.send('Welcome to my awesome app!')
+})
 
 app.listen(3000, function () {
-  console.log('app listening on port 3000');
-});
+  console.log('app listening on port 3000')
+})
